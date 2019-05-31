071f635bfef657592c66bea262fe964e34ce1488
cdesilva <chris.a.desilva@gmail.com>
2019-05-31 11:27:16 -0500

    Remove Fade from Logobanner

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
-import Fade from 'react-reveal/Fade'
 import Logobanner from './Logobanner'
 
 const Navbar = () => {
 
   return (
     <div>
-    <Fade><Logobanner /></Fade>
+    <Logobanner />
         <Menu id="navbar" fluid widths={4} inverted borderless>
           <Menu.Item fitted to='/'><Link to='/'>Home</Link></Menu.Item>
           <Menu.Item fitted><Link to='/about'>About</Link></Menu.Item>
